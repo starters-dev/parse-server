034b576b46026f03abd75241cdd40484a069efa0
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-04-13 16:54:30 +0200

    functions removed

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -3,11 +3,3 @@ const Parse = require('parse/node');
 Parse.Cloud.define('hello', async req => {
   return 'hello';
 });
-
-Parse.Cloud.define('hello2', async req => {
-  return 'hello2';
-});
-
-Parse.Cloud.define('hello3', async req => {
-  return 'hello3';
-});
